b8634125452f912e39b1127bb48961e627e4ce31
sadafes <sadafesmaeili@yahoo.com>
2025-05-12 10:08:09 +0300

    move fetch to DiceButton

--- src/components/DiceButton.jsx
+++ src/components/DiceButton.jsx
@@ -1,20 +1,34 @@
-import diceIcon from '../assets/icon-dice.svg';
-const DiceButton = ({ onClick, loading }) => {
-    return (
-<button
-  onClick={onClick}
-  className={`absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 bg-neon-green w-16 h-16 rounded-full flex items-center justify-center transition hover:shadow-[0_0_20px_theme(colors.neon-green)] ${
-    loading ? "opacity-50 cursor-not-allowed" : ""
-  }`}
-  disabled={loading}
->
-  <span className={`block w-8 h-8 bg-no-repeat bg-center bg-contain ${loading ? "animate-spin" : ""}`}
-    style={{backgroundImage: `url("${diceIcon}")` }}
-  />
+import { useEffect } from "react";
+import diceIcon from "../assets/icon-dice.svg";
 
-</button>
-
-    );
+const DiceButton = ({ onAdviceFetched, setLoading, loading }) => {
+  const fetchAdvice = async () => {
+    setLoading(true);
+    const res = await fetch("https://api.adviceslip.com/advice", { cache: "no-cache" });
+    const data = await res.json();
+    onAdviceFetched(data.slip.advice, data.slip.id);
   };
-  export default DiceButton;
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  return (
+    <button
+      onClick={fetchAdvice}
+      className={`absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 bg-neon-green w-16 h-16 rounded-full flex items-center justify-center transition hover:shadow-[0_0_20px_theme(colors.neon-green)] ${
+        loading ? "opacity-50 cursor-not-allowed" : ""
+      }`}
+      disabled={loading}
+    >
+      <span
+        className={`block w-8 h-8 bg-no-repeat bg-center bg-contain ${loading ? "animate-spin" : ""}`}
+        style={{ backgroundImage: `url("${diceIcon}")` }}
+      />
+    </button>
+  );
+};
+
+export default DiceButton;
+
   
